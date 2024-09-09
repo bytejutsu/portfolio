1aff737ec43a97780f6bd1dc458be15ace324b6e
Dhia Nabli <bytejutsuproject@gmail.com>
2024-09-09 12:28:56 +0100

    improved the reponsiveness of the testimonials

--- src/components/TestimonialsSection/Testimonials.js
+++ src/components/TestimonialsSection/Testimonials.js
@@ -3,13 +3,13 @@ import Testimonial from "@/components/TestimonialsSection/Testimonial";
 export default function Testimonials(){
     return (
         <div className="flex flex-wrap">
-            <div className="lg:pt-12 pt-6 w-full md:w-4/12 px-4 text-center">
+            <div className="lg:pt-12 pt-6 w-full lg:w-4/12 px-4 text-center">
                 <Testimonial/>
             </div>
-            <div className="w-full md:w-4/12 px-4 text-center">
+            <div className="w-full lg:w-4/12 px-4 text-center">
                 <Testimonial/>
             </div>
-            <div className="pt-6 w-full md:w-4/12 px-4 text-center">
+            <div className="pt-6 w-full lg:w-4/12 px-4 text-center">
                 <Testimonial/>
             </div>
         </div>
