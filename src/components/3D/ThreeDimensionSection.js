debc5a178cc430536fd47ab01e7021ee9d07bac5
Dhia Nabli <bytejutsuproject@gmail.com>
2024-09-12 16:35:54 +0100

    made the background black for the 3d section

--- src/components/3D/ThreeDimensionSection.js
+++ src/components/3D/ThreeDimensionSection.js
@@ -5,7 +5,7 @@ const Spline = React.lazy(() => import('@splinetool/react-spline'));
 
 export default function ThreeDimensionSection() {
     return (
-        <section className="bg-primary min-h-screen min-w-screen w-full h-full flex justify-center items-center">
+        <section className="bg-black min-h-screen min-w-screen w-full h-full flex justify-center items-center">
             <Suspense fallback={<></>}>
                 <Spline scene={getAssetPath('./robot_arm.spline')} />
             </Suspense>
