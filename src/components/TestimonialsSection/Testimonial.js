3302251989ea78e2e3ffac499395342a0ccfd33e
Dhia Nabli <bytejutsuproject@gmail.com>
2024-09-13 17:16:00 +0100

    improved the design of the testimonial

--- src/components/TestimonialsSection/Testimonial.js
+++ src/components/TestimonialsSection/Testimonial.js
@@ -4,10 +4,10 @@ const Testimonial = ({name, position, company, content}) => {
     return (
         <div className={styles.wrapper}>
             <div className={`font-extrabold ${styles.title}`}>
-                {name} <small className="text-xl font-light">{position} - <span className="font-bold">{company}</span></small>
+                {name} <small className="text-lg font-light font-mono">{position} - <span className="font-bold text-sm text-gray-800 font-sans2">{company}</span></small>
             </div>
             <div className={styles.description}>
-                <p className="font-semibold">
+                <p className="font-bold">
                     {content}
                 </p>
             </div>
