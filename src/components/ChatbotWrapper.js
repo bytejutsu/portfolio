6a47fcdf46cefab14a7ef69bc61519bfd6aa039f
Dhia Nabli <bytejutsuproject@gmail.com>
2024-09-20 14:00:12 +0100

    improved the chat process in the chatbot

--- src/components/ChatbotWrapper.js
+++ src/components/ChatbotWrapper.js
@@ -42,58 +42,40 @@ export default function ChatbotWrapper() {
         }
     };
 
-    /*
-    const flow = {
-        start: {
-            message: "Hello, I am Apex 👋! The Chatbot of ByteJutsu, I'm excited that you are here 😊! Do you want to know more about the services of ByteJutsu ?",
-            options: ["Yes", "No"],
-            path: "introduction"
-        },
-        introduction: {
-            message: "ByteJutsu is a Software and IT Services company that specializes in providing custom and business tailored software solutions and consulting",
-            path: "end"
-        },
-        end: {
-            message: "Thank you for visiting ! please feel free to contact us if you have any questions",
-            chatDisabled: true
-        }
-    }
-     */
-
     const flow={
         start: {
             message: "Hello there! What is your name?",
             function: (params) => setForm({...form, name: params.userInput}),
-            path: "ask_age"
+            path: "ask_role"
         },
-        ask_age: {
-            message: (params) => `Nice to meet you ${params.userInput}, what is your age?`,
-            function: (params) => setForm({...form, age: params.userInput}),
-            path: "ask_pet"
+        ask_role: {
+            message: (params) => `Nice to meet you ${params.userInput}, what is your current role?`,
+            function: (params) => setForm({...form, role: params.userInput}),
+            path: "ask_customer"
         },
-        ask_pet: {
-            message: "Do you own any pets?",
+        ask_customer: {
+            message: "Are you already a customer of ByteJutsu?",
             // alternative way to declare options, with sending of output disabled
             // more info here: https://react-chatbotify.com/docs/api/attributes
             // options: {items: ["Yes", "No"], sendOutput: false}
             options: ["Yes", "No"],
             chatDisabled: true,
-            function: (params) => setForm({...form, pet_ownership: params.userInput}),
+            function: (params) => setForm({...form, already_customer: params.userInput}),
             path: "ask_choice"
         },
         ask_choice: {
-            message: "Select at least 2 pets that you are comfortable to work with:",
+            message: "Let me know what services are you interested in",
             // alternative way to declare checkboxes, with default configurations (i.e. min 1, max 4, send output and not reusable)
             // more info here: https://react-chatbotify.com/docs/api/attributes
             // checkboxes: ["Dog", "Cat", "Rabbit", "Hamster"]
-            checkboxes: {items: ["Dog", "Cat", "Rabbit", "Hamster"], min: 2},
+            checkboxes: {items: ["Consulting", "E-commerce", "Web App", "Mobile App", "Wordpress", "Prestashop", "Digital Marketing" ,"Custom Solution"], min: 1},
             chatDisabled: true,
-            function: (params) => setForm({...form, pet_choices: params.userInput}),
-            path: "ask_work_days"
+            function: (params) => setForm({...form, services_choices: params.userInput}),
+            path: "ask_budget"
         },
-        ask_work_days: {
-            message: "How many days can you work per week?",
-            function: (params) => setForm({...form, num_work_days: params.userInput}),
+        ask_budget: {
+            message: "what is your budget?",
+            function: (params) => setForm({...form, budget: params.userInput}),
             path: "end"
         },
         end: {
@@ -101,10 +83,10 @@ export default function ChatbotWrapper() {
             component: (
                 <div style={formStyle}>
                     <p>Name: {form.name}</p>
-                    <p>Age: {form.age}</p>
-                    <p>Pet Ownership: {form.pet_ownership}</p>
-                    <p>Pet Choices: {form.pet_choices}</p>
-                    <p>Num Work Days: {form.num_work_days}</p>
+                    <p>Role: {form.role}</p>
+                    <p>Already Customer: {form.already_customer}</p>
+                    <p>Interested in: {form.services_choices}</p>
+                    <p>Budget: {form.budget}</p>
                 </div>
             ),
             options: ["New Application"],
